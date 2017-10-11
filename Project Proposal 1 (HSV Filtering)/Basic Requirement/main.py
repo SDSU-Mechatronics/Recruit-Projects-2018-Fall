5bc9c6394115df338999c259d1adb251134654fb
DESKTOP-MQI17QV\justi <justinseitz2016@gmail.com>
2017-10-10 19:07:55 -0700

    Added canny edge, Hough circles, and Hough circles with HSV filtering

--- Project Proposal 1 (HSV Filtering)/Basic Requirement/main.py
+++ Project Proposal 1 (HSV Filtering)/Basic Requirement/main.py
@@ -1,39 +1,78 @@
 import cv2
 import numpy
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    b, source = cap.read()
-    
+def HSV_Filter(source):
     frame = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
     
-    width, height, channels = frame.shape
-    
-    hsvMin = numpy.array([80, 0, 0])
-    hsvMax = numpy.array([125, 255, 255])
+    hsvMin = numpy.array([64, 128, 0])
+    hsvMax = numpy.array([128, 255, 255])
     
     mask = cv2.inRange(frame, hsvMin, hsvMax)
     
     frame = cv2.bitwise_and(frame, frame, mask = mask)
 
-    
-    '''
-    for x in range(width):
-        for y in range(height):
-            if frame[x][y][0] > 80 and frame[x][y][0] < 125:
-                pass
-            else:
-                frame[x][y][0] = 0
-                frame[x][y][1] = 0
-                frame[x][y][2] = 0
-    '''
-    
     frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
     
+    return frame
+
+def Canny_Edge(source):
+    frame = cv2.Canny(source, 100, 200)
+    return frame
+
+
+def Hough_Circles(source):
+    output = source.copy()
+    
+    gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+    
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.4, 100)
+    
+    if circles is not None:
+        for val in circles:
+            cv2.circle(output, (val[0][0], val[0][1]), val[0][2], (0, 255, 0), 4)
+    
+    return output
+
+
+def Hough_Circles_HSV(source):
+    frame = HSV_Filter(source)
+    
+    output = source.copy()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2, 100)
+    
+    if circles is not None:
+        for val in circles:
+            cv2.circle(output, (val[0][0], val[0][1]), val[0][2], (0, 255, 0), 4)
+    
+    return output
+
+def Contour_Analysis(source):
+    output = source.copy()
+    '''
+    ret, thresh = cv2.threshold(Canny_Edge(output), 1, 1, 1)
+    
+    _, contours, heirarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    '''
+    
+    return output
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    b, source = cap.read()
+    
+    cv2.imshow("Filter", HSV_Filter(source))
     cv2.imshow("Source", source)
-    cv2.imshow("Filtered", frame)
-    if(cv2.waitKey(10) &0xFF == ord('q')):
+    cv2.imshow("Canny Edge", Canny_Edge(source))
+    #cv2.imshow("Hough Circles", Hough_Circles(source))
+    cv2.imshow("Hough Circles HSV", Hough_Circles_HSV(source))
+    #cv2.imshow("Contour Analysis", Contour_Analysis(source))
+    
+    if (cv2.waitKey(100) &0xFF == ord('q')):
         break
-        
+    
 cap.release()
