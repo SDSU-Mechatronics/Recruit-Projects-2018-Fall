8a99d1704e07f6488866d5ed6714a107fdd25be8
Cole <cole.brower@gmail.com>
2017-10-22 12:23:08 -0700

    [*] Rotates a server

---
+++ Project Proposal 2 (Arduino Project)/Basic Requirement/basicRequirement/basicRequirement.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+
+int pinIn = 0;
+int pinOut = 3;
+int degree = 0;
+
+String output = "Hello, World!";
+int length = 0;
+int gotInput = 0;
+
+Servo serv;
+
+void setup(){
+  pinMode(pinOut, OUTPUT);
+  Serial.begin(9600);
+  serv.attach(3);
+}
+
+void loop() {
+  while (Serial.available()){
+    char input = Serial.read();
+    
+    if(input == ';'){
+      gotInput = 1;
+      output = "";
+      break;
+    }
+    output += input;
+  } 
+  //Serial.println(output);
+  degree += 20;
+  if (degree > 180){
+    degree = 0;
+  }
+  Serial.println(degree);
+  serv.write(degree);
+  delay(250);
+}
