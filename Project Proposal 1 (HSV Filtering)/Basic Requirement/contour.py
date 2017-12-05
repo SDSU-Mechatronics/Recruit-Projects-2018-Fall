2929f90cc0898e36af1bb50c485c451a2e17c0ba
tortelllini <trenton.green@sbcglobal.net>
2017-12-05 13:18:52 -0800

    Update contour.py

--- Project Proposal 1 (HSV Filtering)/Basic Requirement/contour.py
+++ Project Proposal 1 (HSV Filtering)/Basic Requirement/contour.py
@@ -6,16 +6,29 @@ Created on Oct 20, 2017
 
 import cv2
 import numpy as np
-image = cv2.imread('switch.jpg')
-blurred = cv2.pyrMeanShiftFiltering(image,31,91)
-gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-ret, threshold = cv2.threshold(gray,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+from numpy import shape
 
-_, contours,_ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-print "Number of contours detected %d -> " %len(contours)
+raw_image = cv2.imread('switch.jpg')
+cv2.imshow('Original Image', raw_image)
+cv2.waitKey(0)
 
-cv2.drawContours(image, contours, 0,(0,0,255),6)
+bilateral_filtered_image = cv2.bilateralFilter(raw_image, 5, 175, 175)
+cv2.imshow('Bilateral', bilateral_filtered_image)
+cv2.waitKey(0)
 
-cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
-cv2.imshow('Display',image)
-cv2.waitKey() 
+edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+cv2.imshow('Edge', edge_detected_image)
+cv2.waitKey(0)
+
+contours, hierarchy = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contour_list = []
+for contour in contours:
+    approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
+    area = cv2.contourArea(contour)
+    if ((len(approx) > 8) & (len(approx) < 23) & (area > 30) ):
+        contour_list.append(contour)
+
+cv2.drawContours(raw_image, contour_list,  -1, (255,0,0), 2)
+cv2.imshow('Objects Detected',raw_image)
+cv2.waitKey(0)
